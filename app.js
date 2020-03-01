3a0aaff49009fdf32b3a082e3093a370fea19cf8
Gally <gally999@free.fr>
2020-03-01 16:20:16 +0100

    fix deprecated errors in back

--- app.js
+++ app.js
@@ -14,7 +14,11 @@ const passport     = require("passport");
 require("./config/passport-setup.js");
 
 mongoose
-  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
+  .set('useCreateIndex', true)
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
